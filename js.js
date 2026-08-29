8a3c73c843b78f2298f253432d5250876962aefd
Narayansethi2026 <sethinarayan674@gmail.com>
2026-08-29 09:23:12 +0530

    added password hide show project

--- js.js
+++ js.js
@@ -6,10 +6,26 @@
 
 //    heading.textContent = textinput.value
 // });
-let heading = document.getElementById("heading");
-let btn = document.getElementById("btn")
+// let heading = document.getElementById("heading");
+// let btn = document.getElementById("btn")
 
-btn.addEventListener("click", function () {
-  heading.innerText = "thank you bro";
-  heading.innerHTML = "chal nikal"
+// btn.addEventListener("click", function () {
+//   heading.innerText = "thank you bro";
+//   heading.innerHTML = "chal nikal"
+// });
+let password = document.getElementById("password");
+let showBtn = document.getElementById("showBtn");
+let message = document.getElementById("message");
+
+showBtn.addEventListener("click",function () {
+  if (password.type === "password") {
+    
+      password.type = "text";
+      showBtn.textContent = "hide";
+  }
+  else {
+    password.type = "password";
+    showBtn.textContent = "show";
+  }
+
 });
