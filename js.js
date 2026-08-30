a425f81aae6238ece9f411c2f950a3bd6cb66986
Narayansethi2026 <sethinarayan674@gmail.com>
2026-08-30 13:46:21 +0530

    create a milage calculate project with java script

--- js.js
+++ js.js
@@ -13,19 +13,63 @@
 //   heading.innerText = "thank you bro";
 //   heading.innerHTML = "chal nikal"
 // });
-let password = document.getElementById("password");
-let showBtn = document.getElementById("showBtn");
-let message = document.getElementById("message");
+// password hide show project
+// let password = document.getElementById("password");
+// let showBtn = document.getElementById("showBtn");
+// let message = document.getElementById("message");
 
-showBtn.addEventListener("click",function () {
-  if (password.type === "password") {
+// showBtn.addEventListener("click",function () {
+//   if (password.type === "password") {
     
-      password.type = "text";
-      showBtn.textContent = "hide";
-  }
-  else {
-    password.type = "password";
-    showBtn.textContent = "show";
-  }
+//       password.type = "text";
+//       showBtn.textContent = "hide";
+//       document.body.style.backgroundColor = "red";
+//   }
+//   else {
+//     password.type = "password";
+//     showBtn.textContent = "show";
+//     document.body.style.backgroundColor = "white"
+//   }
 
-});
+// });
+// let red = document.getElementById("red");
+// let orange = document.getElementById("orange")
+// let blue = document.getElementById("blue")
+
+// red.addEventListener("click", function () {
+//   document.body.style.backgroundColor = "red"
+// });
+
+// orange.addEventListener("click", function () {
+//   document.body.style.backgroundColor = "blue"
+// });
+
+// blue.addEventListener("keydown",function () {
+//   document.body.style.backgroundColor = "orange"
+// });
+// let input = document.getElementById("input");
+// let button = document.getElementById("btn");
+// let box = document.getElementById("box");
+
+// button.addEventListener("click", function () {
+//   let color = input.value;
+//   box.style.backgroundColor = color;
+//   message.innertext = "background color:" + color;
+
+// });
+// let distance = 300;
+// let petrol = 10;
+
+// let mileage = distance / petrol;
+// console.log(mileage);
+// let distance = document.getElementById("distance");
+// let fule = document.getElementById("fuel");
+// let button = document.getElementById("calculate");
+// let result = document.getElementById("result");
+// button.addEventListener("click", function() {
+//     let km = Number(distance.value);
+//     let litre = Number(fule.value);
+
+//     let mileage = km / litre;
+//     result.innerText = "mileage = " + mileage + "km/L";
+// });
