b26994420673e8670222b27da465fa13d133b812
Narayansethi2026 <sethinarayan674@gmail.com>
2026-09-07 22:37:08 +0530

    age calulatetor

--- js.js
+++ js.js
@@ -1,120 +1,27 @@
-// let student = [];
-
-// let addBtn = document.getElementById("addBtn");
-// let nameInput = document.getElementById("name");
-// let mathInput = document.getElementById("maths");
-// let englishInput = document.getElementById("english");
-// let scienceInput = document.getElementById("science");
-
-// let result = document.getElementById("result");
-
-// addBtn.addEventListener("click", function () {
-//     let name = nameInput.value;
-
-//     let maths = Number(mathInput.value);
-//     let english = Number(englishInput.value);
-//     let computer = Number(scienceInput.value);
-
-//     let student = {
-//         name: name,
-//         maths: maths,
-//         english: english,
-//         computer: computer
-//     };
-
-//     student.push(student);
-
-//     displayStudent();
-
-// });
-
-// function displayStudent() {
-//     result.innerHTML = "";
-
-//     student.forEach(function (student) {
-
-//         let total = student.maths +
-//             student.english +
-//             student.computer;
-//         let percentage = total / 3;
-
-//         let card = document.createElement("div");
-
-//         card.classList.add("student-card");
-
-//         card.innerHTML = `
-//               <h2>${student.name}<h2>
-//               <p>maths: ${student.maths}</p>
-//               <p>english: ${student.english}</p>
-//               <p>science: ${student.computer}</p>
-//               <p>total: ${total}</p>
-//               <p>percentage: ${percentage}%</p>`;
-//               result.append(card);
-//     });
-// }
-let students = [];
-
-let addBtn = document.getElementById("addBtn");
-let nameInput = document.getElementById("name");
-let mathInput = document.getElementById("maths");
-let englishInput = document.getElementById("english");
-let scienceInput = document.getElementById("science");
-
+let dob = document.getElementById("dob");
+let calculateBtn = document.getElementById("calculateBtn");
 let result = document.getElementById("result");
 
+calculateBtn.addEventListener("click",function() {
 
-addBtn.addEventListener("click", function () {
+    if(dob.value ==="") {
 
-    let name = nameInput.value;
+        result.textContent = "please selete your date of birth";
+        return;
+    }
+    
+   let birthDate = new Date(dob.value);
+   let today = new Date(); 
+   let age = today.getFullYear() - birthDate.getFullYear();
 
-    let maths = Number(mathInput.value);
-    let english = Number(englishInput.value);
-    let computer = Number(scienceInput.value);
+   let month = today.getMonth() - birthDate.getMonth();
 
+   if(
+    month < 0 || 
+    (month === 0 && today.getDate() < birthDate.getDate())
 
-    let student = {
-        name: name,
-        maths: maths,
-        english: english,
-        computer: computer
-    };
-
-
-    students.push(student);
-
-    displayStudents();
+   ){
+    age--;
+   }
+   result.textContent = "your age is: " + age + "year";
 });
-
-
-function displayStudents() {
-
-    result.innerHTML = "";
-
-    students.forEach(function (student) {
-
-        let total =
-            student.maths +
-            student.english +
-            student.computer;
-
-        let percentage = total / 3;
-
-
-        let card = document.createElement("div");
-
-        card.classList.add("students-card");
-
-
-        card.innerHTML = `
-            <h2>${student.name}</h2>
-            <p>Maths: ${student.maths}</p>
-            <p>English: ${student.english}</p>
-            <p>Science: ${student.computer}</p>
-            <p>Total: ${total}</p>
-            <p>Percentage: ${percentage}%</p>
-        `;
-
-
-        result.append(card);
-    });
-}
