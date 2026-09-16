f620b838061829f364c89cdec49332789141cabd
Narayansethi2026 <sethinarayan674@gmail.com>
2026-09-16 23:23:32 +0530

    pin and email valid or not valid code

--- js.js
+++ js.js
@@ -1,27 +1,16 @@
-let dob = document.getElementById("dob");
-let calculateBtn = document.getElementById("calculateBtn");
-let result = document.getElementById("result");
+function validate() {
+    var email = document.getElementById("email").value;
 
-calculateBtn.addEventListener("click",function() {
-
-    if(dob.value ==="") {
-
-        result.textContent = "please selete your date of birth";
-        return;
+    if (email == "") {
+        alert("email cannot be empty");
     }
-    
-   let birthDate = new Date(dob.value);
-   let today = new Date(); 
-   let age = today.getFullYear() - birthDate.getFullYear();
-
-   let month = today.getMonth() - birthDate.getMonth();
-
-   if(
-    month < 0 || 
-    (month === 0 && today.getDate() < birthDate.getDate())
-
-   ){
-    age--;
-   }
-   result.textContent = "your age is: " + age + "year";
-});
+    else if (!email.include("@")) {
+        alert("invalid email");
+    }
+    else if (!email.include(".")) {
+        alert("invalid email");
+    }
+    else {
+        alert("valid email")
+    }    
+}
