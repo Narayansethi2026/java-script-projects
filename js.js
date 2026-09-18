700ef9401039e7b2d0178b4e70b197b87a82019c
Narayansethi2026 <sethinarayan674@gmail.com>
2026-09-18 23:26:08 +0530

    reverseString

--- js.js
+++ js.js
@@ -1,11 +1,18 @@
-function removeSpace() {
-    let str = document.getElementById("text").value;
-    let result = str . replace(/\s/g, "");
-    document.getElementById("result").innerHTML = result;
-}
+// function removeSpace() {
+//     let str = document.getElementById("text").value;
+//     let result = str . replace(/\s/g, "");
+//     document.getElementById("result").innerHTML = result;
+// }
 
 // function removeSpace() {
 //     let str = document.getElementById("text").value;
 //     let result = str . replace(/\s/g, "");
 //     document.getElementById("result").innerHTML = result;
 // }
+
+
+function reverseString() {
+    let str = document.getElementById("txt").value;
+    let rev = str.split("").reverse().join("");
+    document.getElementById("result").innerHTML = rev;
+}
